42a9993569b44fa292fb71e27e3e179d20784ce0
Phạm Tuyên <phamthanhtuyen2k8@gmail.com>
2023-10-03 10:51:15 +0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Container } from "react-bootstrap"
+import { useState } from 'react';
+import quiz from "./quiz.json"
+
+function Ques(props) {
+  return(<h3>{props.text}</h3>)
+}
+
+function Ans(props) {
+  return (<Button ans={props.name} className='ms-4' onClick={props.click}>{props.text}</Button>)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var [i, setI] = useState(0)
+  const check = (i, ans, setI) => {
+    if(quiz[i].right === ans) alert("Đúng rồi!!!")
+    else alert(`Sai rồi. Đáp án là ${quiz[i][quiz[i].right]}`)
+    setI(i + 1)
+  }
+  if(i !== (quiz.length - 1)) {
+    return (
+      <Container className='mt-5 pt-5'>
+        <Ques text={quiz[i].ques}/>
+        <div className='d-flex justify-content-center mt-5'>
+          <Ans name={"a"} text={quiz[i].a} click={() => check(i, "a", setI)}/>
+          <Ans name={"b"} text={quiz[i].b} click={() => check(i, "b", setI)}/>
+          <Ans name={"c"} text={quiz[i].c} click={() => check(i, "c", setI)}/>
+          <Ans name={"d"} text={quiz[i].d} click={() => check(i, "d", setI)}/>
+        </div>
+     
+      </Container>
+    )
+  } else return (<h1 className='d-flex justify-content-center'>Hết câu hỏi</h1>)
 }
 
 export default App;
